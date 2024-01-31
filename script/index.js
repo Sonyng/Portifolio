4fe8b076cae767c844309db15ea81a441b4a7627
Vinicius <viniauthentic@gmail.com>
2024-01-30 21:04:34 -0300

    up

--- script/index.js
+++ script/index.js
@@ -1,29 +1,41 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-        }
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function () {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('.navbar a');
     let menuIcon = document.getElementById('menu_icon');
     let navbar = document.querySelector('.navbar');
 
-    menuIcon.addEventListener('click', () => {
-        // Alternar a classe 'responsive-navbar' na barra de navegação
-        navbar.classList.toggle('responsive-navbar');
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                document.querySelector('.navbar a[href*="' + id + '"]').classList.add('active');
+            }
+        });
+    }
+
+    menuIcon.addEventListener('click', function (e) {
+        e.preventDefault();
+        navbar.classList.toggle('show');
+    });
+
+    // Adiciona o seguinte código para fechar o menu ao clicar em qualquer lugar fora dele
+    document.addEventListener('click', function (event) {
+        if (!event.target.closest('.navbar') && !event.target.closest('#menu_icon')) {
+            navbar.classList.remove('show');
+        }
+    });
+
+    // Adiciona o seguinte código para fechar o menu ao pressionar a tecla Esc
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape' && navbar.classList.contains('show')) {
+            navbar.classList.remove('show');
+        }
     });
 });
-
